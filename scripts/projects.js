13fe07c435ce37b6b10dc12846a6a49afb07c8f6
Floris Louërat <floris.louerat@gmail.com>
2023-11-24 19:19:45 +0100

    + prjects tools

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,15 +1,23 @@
 const portfolio = document.getElementById('portfolio');
 
 class Project {
-    constructor(title, image, description, link) {
+    constructor(title, image, description, link, tools) {
         this.title = title;
         this.image = image;
         this.description = description;
         this.link = link;
+        this.tools = tools;
 
         this.createProjectElement();
     }
     createProjectElement() {
+
+        this.mainPart = document.createElement('div');
+        this.mainPart.classList.add('projectMain');
+
+        this.toolsPart = document.createElement('div');
+        this.toolsPart.classList.add('projectTools');
+
         this.Pcard = document.createElement('div');
         this.Pcard.classList.add('project');
 
@@ -20,13 +28,24 @@ class Project {
         this.Pimg.src = this.image;
         this.Pimg.alt = `image de ${this.title}`;
 
-
         this.Pdesc = document.createElement('p');
         this.Pdesc.innerHTML = this.description;
 
-        this.Pcard.appendChild(this.Pname);
-        this.Pcard.appendChild(this.Pimg);
-        this.Pcard.appendChild(this.Pdesc);
+        for (let tool of this.tools) {
+            this.toolImg = document.createElement('img');
+            this.toolImg.src = `./src/tools/${tool}.png`;
+            this.toolImg.alt = tool;
+
+            this.toolsPart.appendChild(this.toolImg);
+        }
+
+
+        this.mainPart.appendChild(this.Pname);
+        this.mainPart.appendChild(this.Pimg);
+        this.mainPart.appendChild(this.Pdesc);
+
+        this.Pcard.appendChild(this.mainPart);
+        this.Pcard.appendChild(this.toolsPart);
 
         portfolio.appendChild(this.Pcard);
 
@@ -40,14 +59,16 @@ new Project(
     'Pokédex',
     './src/projects/pokedex.png',
     'un pokedex',
-    'https://flozerty.github.io/Flokedex-JSavance/'
+    'https://flozerty.github.io/Flokedex-JSavance/',
+    ['html', 'js']
 )
 
 new Project(
     'Gaming Place',
     './src/projects/gamingplace.png',
     'un site vitrine de créateurs de jeux',
-    'https://flozerty.github.io/TP-JS/'
+    'https://flozerty.github.io/TP-JS/',
+    ['html', 'sass', 'js']
 
 )
 
@@ -55,20 +76,23 @@ new Project(
     'Ace-Attorney Remaster',
     './src/projects/aceattorney.png',
     'un remaster du jeu Ace Attorney',
-    'https://flozerty.github.io/ace-attorney-remaster/'
+    'https://flozerty.github.io/ace-attorney-remaster/',
+    ['js']
+
 )
 
 new Project(
     'Portfolio',
     './src/projects/portfolio.png',
     'Le portfolio que vous voyez ici présent!',
-    'https://flozerty.github.io/portfolio/'
+    'https://flozerty.github.io/portfolio/',
+    ['html', 'sass', 'js']
 )
 
 new Project(
     'PlanetBodies.com',
     './src/projects/planetBodies.png',
     'Un site de référencement des satellites de notre système',
-    'https://flozerty.github.io/TP-Angular/'
-
+    'https://flozerty.github.io/TP-Angular/',
+    ['angular', 'sass', 'ts']
 )
